769c615b248ba00eade0881c1fee36cfc83b7947
Christian Ferbar <christian.ferbar@gmx.net>
2017-02-12 14:42:31 +0000

    cleanup, add comments

--- RuntimeExceptionWithBacktrace.cpp
+++ RuntimeExceptionWithBacktrace.cpp
@@ -14,9 +14,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-// dirname
-#include <libgen.h>
-
 #undef runtime_error
 std::RuntimeExceptionWithBacktrace::RuntimeExceptionWithBacktrace(const std::string &what)
 	: std::runtime_error(what)
@@ -24,23 +21,17 @@ std::RuntimeExceptionWithBacktrace::RuntimeExceptionWithBacktrace(const std::str
 	void *array[10];
 	size_t size;
 
+	/* --- print simple backtrace
 	// get void*'s for all entries on the stack
 	size = backtrace(array, 10);
 
 	// print out all the frames to stderr das kamma mit addr2line noch anschaun dann:
 	fprintf(stderr, "Error: %s size=%zd\n", what.c_str(), size);
 	backtrace_symbols_fd(array, size, STDERR_FILENO);
-	/*
-	messages = backtrace_symbols(array, size);
-
-	// skip first stack frame (points here)
-	for (i = 1; i < size && messages != NULL; ++i) {
-		fprintf(stderr, "[bt]: (%d) %s\n", i, messages[i]);
-	}
-	free(messages);
-
 	*/
 
+
+	// Print Backtrace with demangle: - needs LDFLAGS -rdynamic
 	// http://stackoverflow.com/questions/77005/how-to-generate-a-stacktrace-when-my-gcc-c-app-crashes
 
 	char ** messages = backtrace_symbols(array, size);    
@@ -48,8 +39,14 @@ std::RuntimeExceptionWithBacktrace::RuntimeExceptionWithBacktrace(const std::str
 	// skip first stack frame (points here)
 	for (size_t i = 1; i < size && messages != NULL; ++i) {
 		char *mangled_name = 0, *offset_begin = 0, *offset_end = 0;
+		// char *address_begin = 0, *address_end = 0;
 
 		// find parantheses and +address offset surrounding mangled name
+		// normalerweise sollte messages[i] so ausschaun: ./listing1(show_stackframe+0x2e) [0x80486de]
+		// ohne -rdynamic fehlt der (show_stackframe+0x2e) - Teil!
+		// addr2line verwendet die libdwarf, aber das dürften 300 zeilen benötigen
+		// dladdr liefert nicht mehr Information als das backtrace()
+		// LDFLAGS: -rdynamic hilft !!!
 		for (char *p = messages[i]; *p; ++p) {
 			if (*p == '(') {
 				mangled_name = p; 
@@ -58,9 +55,22 @@ std::RuntimeExceptionWithBacktrace::RuntimeExceptionWithBacktrace(const std::str
 			} else if (*p == ')') {
 				offset_end = p;
 				break;
-			}
+			} /* else if (*p == '[') {
+				address_begin = p;
+			} else if (*p == ']') {
+				address_end = p;
+			} */
 		}
-
+		/*
+		if(address_begin && address_end && (address_begin < address_end)) {
+			long addr=strtol(address_begin+1,NULL,0);
+			printf("addr=%#lx\n", addr);
+			Dl_info info;
+			int rc=dladdr((void *) addr, &info);
+			printf("rc=%d, %s %p\n", rc, info.dli_fname, info.dli_saddr);
+		}
+		*/
+		// printf("mangled_name %s=%p offset_begin %p offset_end %p \n", mangled_name, mangled_name, offset_begin, offset_end);
 		// if the line could be processed, attempt to demangle the symbol
 		if (mangled_name && offset_begin && offset_end && mangled_name < offset_begin) {
 			*mangled_name++ = '\0';
